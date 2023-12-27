f0eac639aefb8b9a52c95a448f08e0e149a943f7
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2023-12-27 19:38:15 +0530

    fix: Updated the wrong script

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-if [ -t 1 ]; then
-  DOCKER_OPTS="-it"
-else
-  # Disable interactive mode in CI
-  DOCKER_OPTS=""
-fi
-
-docker run $DOCKER_OPTS kapilgain/bootstrap "$@"
+docker build -t kapilgain/bootstrap .
--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-docker run -it kapilgain/bootstrap "$@"
+if [ -t 1 ]; then
+  DOCKER_OPTS="-it"
+else
+  # Disable interactive mode in CI
+  DOCKER_OPTS=""
+fi
+
+docker run $DOCKER_OPTS kapilgain/bootstrap "$@"
